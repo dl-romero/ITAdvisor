72c2bdf41f9035f939c86672ab76661159b0ee42
David Romero <dromero@qualcomm.com>
2021-01-15 13:45:54 -0800

    Update itadvisor.py

--- itadvisor.py
+++ itadvisor.py
@@ -39,15 +39,15 @@ class ITAdvisor:
         self.UserMessage = self.UserMessage(hostname = self.hostmame, http_auth = self.http_auth)
         self.Users = self.Users(hostname = self.hostmame, http_auth = self.http_auth)
         self.WorkOrders = self.WorkOrders(hostname = self.hostmame, http_auth = self.http_auth)
-        
+
     class Assets:
         '''Asset Library'''
         def __init__(self, hostname, http_auth):
             '''init'''
             self.hostmame = hostname
             self.http_auth = http_auth
-            
-        def Query(self, query):
+
+        def search(self, query):
             '''
             Searches for assets containing the words in the string.
             The search functionality is viable for searching specific rooms, locations or assets. 
@@ -56,6 +56,20 @@ class ITAdvisor:
             '''
             requests.packages.urllib3.disable_warnings()
             url = "https://" +str(self.hostmame)+ "/api/current/assets/search?q=" + str(query)
+            payload={}
+            headers = {}
+            try:
+                response = requests.request("GET", url, headers=headers, data=payload, verify=False, auth=self.http_auth)
+                if response.status_code != 200:
+                    return response.status_code
+                return response.text
+            except requests.exceptions.ConnectionError:
+                return '{\'status\':\'0\', \'description\':\'unreachable\'}'
+
+        def types(self):
+            '''The types describe how the assets are presented in the EcoStruxure IT Advisor client.'''
+            requests.packages.urllib3.disable_warnings()
+            url = "https://" +str(self.hostmame)+ "/api/current/assets/types"
             payload={}
             headers = {}
             try:
@@ -159,10 +173,39 @@ class ITAdvisor:
         
     class Genomes:
         '''Genomes Library'''
+        # Complete
         def __init__(self, hostname, http_auth):
             '''init'''
             self.hostmame = hostname
             self.http_auth = http_auth
+            
+        def all_genomes(self):
+            '''Returns all Genomes in the System and User Genome Libraries.'''
+            requests.packages.urllib3.disable_warnings()
+            url = "https://" +str(self.hostmame)+ "/api/current/genomes"
+            payload={}
+            headers = {}
+            try:
+                response = requests.request("GET", url, headers=headers, data=payload, verify=False, auth=self.http_auth)
+                if response.status_code != 200:
+                    return response.status_code
+                return response.text
+            except requests.exceptions.ConnectionError:
+                return '{\'status\':\'0\', \'description\':\'unreachable\'}'
+            
+        def genome_by_id(self, genome_id):
+            '''Returns the single Genome referenced by ID from the System and User Genome Libraries.'''
+            requests.packages.urllib3.disable_warnings()
+            url = "https://" +str(self.hostmame)+ "/api/current/genomes/" + str(genome_id)
+            payload={}
+            headers = {}
+            try:
+                response = requests.request("GET", url, headers=headers, data=payload, verify=False, auth=self.http_auth)
+                if response.status_code != 200:
+                    return response.status_code
+                return response.text
+            except requests.exceptions.ConnectionError:
+                return '{\'status\':\'0\', \'description\':\'unreachable\'}'
         
     class KPIs:
         '''KPIs Library'''
@@ -171,12 +214,56 @@ class ITAdvisor:
             self.hostmame = hostname
             self.http_auth = http_auth
         
-    class Licenses:
+    class Licenses: 
         '''Licenses Library'''
+        # Complete
         def __init__(self, hostname, http_auth):
             '''init'''
             self.hostmame = hostname
             self.http_auth = http_auth
+            
+        def get_licenses(self):
+            '''Returns all installed licenses.'''
+            requests.packages.urllib3.disable_warnings()
+            url = "https://" +str(self.hostmame)+ "/api/current//licenses"
+            payload={}
+            headers = {}
+            try:
+                response = requests.request("GET", url, headers=headers, data=payload, verify=False, auth=self.http_auth)
+                if response.status_code != 200:
+                    return response.status_code
+                return response.text
+            except requests.exceptions.ConnectionError:
+                return '{\'status\':\'0\', \'description\':\'unreachable\'}'
+            
+        def post_license(self, license_key):
+            '''Returns all installed licenses.'''
+            requests.packages.urllib3.disable_warnings()
+            url = "https://" +str(self.hostmame)+ "/api/current/licenses"
+            payload={'-d': str(license_key)}
+            files=[]
+            headers = {}
+            try:
+                response = requests.request("POST", url, headers=headers, data=payload, files=files, verify=False, auth=self.http_auth)
+                if response.status_code != 200:
+                    return response.status_code
+                return response.text
+            except requests.exceptions.ConnectionError:
+                return '{\'status\':\'0\', \'description\':\'unreachable\'}'
+            
+        def delete_license(self, license_key):
+            '''Returns all installed licenses.'''
+            requests.packages.urllib3.disable_warnings()
+            url = "https://" +str(self.hostmame)+ "/api/current/licenses/" + str(license_key)
+            payload={}
+            headers = {}
+            try:
+                response = requests.request("DELETE", url, headers=headers, data=payload, verify=False, auth=self.http_auth)
+                if response.status_code != 200 or response.status_code != 204:
+                    return response.status_code
+                return response.text
+            except requests.exceptions.ConnectionError:
+                return '{\'status\':\'0\', \'description\':\'unreachable\'}'
         
     class Mail:
         '''Mail Library'''
